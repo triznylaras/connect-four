531196ee725cb3ffb553607d7c2956a8deb25a11
triznylaras <triznylaras@gmail.com>
2021-12-28 14:48:35 +0700

    create get_cell, set_cell method

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -13,5 +13,13 @@ module ConnectFour
     def default_grid
       Array.new(6) { Array.new(7) { Cell.new(empty_circle) } }
     end
+
+    def get_cell(x_ax, y_ax)
+      grid[y_ax][x_ax]
+    end
+
+    def set_cell(x_ax, y_ax, value)
+      grid[y_ax][x_ax].value = value
+    end
   end
 end
